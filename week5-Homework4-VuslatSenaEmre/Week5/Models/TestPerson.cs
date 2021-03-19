4a3ecf5d95ef280211de73c0fa69786249bc521b
Vuslat Sena EMRE <47564075+VuslatSena@users.noreply.github.com>
2021-03-19 13:30:09 +0300

    Delete week5-Homework4-VuslatSenaEmre directory

--- week5-Homework4-VuslatSenaEmre/Week5/Models/TestPerson.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Week5.Controllers
-{
-    internal class TestPerson
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-    }
-}
